a7b5a6aede13925a6bbb3da4020f527c21babe89
Fernando Cutire UTP <fernando.cutire@utp.ac.pa>
2020-02-12 22:57:59 -0500

    Ahora si compila

--- src/index.js
+++ src/index.js
@@ -2,4 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 
+import { combineReducers } from "redux";
+import authReducer from "./reducers/authReducers";
+import errorReducer from "./reducers/errorReducers";
+
+export default combineReducers({
+  auth: authReducer,
+  errors: errorReducer
+});
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
