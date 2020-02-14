46722e27f7f32aab790784b3786541896c47a7fe
Fernando Cutire UTP <fernando.cutire@utp.ac.pa>
2020-02-13 19:25:26 -0500

    modificado los 2 de nuevo

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -20,7 +20,7 @@ export const registerUser = (userData, history) => dispatch => {
 // Login - get user token
 export const loginUser = userData => dispatch => {
   axios
-    .post("/api/users/login", userData)
+    .post("https://ieee-backend-utp.herokuapp.com/api/users/login", userData)
     .then(res => {
       // Save to localStorage
 
